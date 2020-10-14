f7539be13c29f79e6367e30d1217f0bc77133f9e
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-10-14 14:16:20 -0500

    added feed query function and return

--- src/index.js
+++ src/index.js
@@ -9,15 +9,18 @@ import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
+//Creates link that will connect to client to graphql api
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
+//instantiaion of client passing in above connection
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
 })
 
+//Wrap app in HOC that expects client as a prop
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
