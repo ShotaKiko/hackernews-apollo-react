4e5d0e81a4fbdb77d440799fc11df341a76a7311
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-10-13 21:36:27 -0500

    installed apollo client and associated packages/configured App in index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import "./styles/index.css";
-import App from "./components/App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import "./styles/index.css"
+import App from "./components/App"
+
+//Apollo imports
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
